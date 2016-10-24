5aca75f47de738ecb6cec138fca7d1d6739cdb04
Mark Stevens <railsadvocate@gmail.com>
2016-10-23 18:04:43 -0700

    added ability to add and edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :get_user_from_params, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -16,8 +19,24 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(whitelisted_user_params)
+      flash[:notice] = "Your account was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def whitelisted_user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def get_user_from_params
+    @user = User.find(params[:id])
   end
 end
