87d47769018d10f1490c6e1696868f9aed4463e8
Mark Stevens <railsadvocate@gmail.com>
2016-10-30 17:51:05 -0700

    increase maximum size of article description

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
     belongs_to :user
     has_many :comments
     validates :title, :presence => true, :length => { :minimum => 3, :maximum => 50 }
-    validates :description, :presence => true, :length => { :minimum => 10, :maximum => 300 }
+    validates :description, :presence => true, :length => { :minimum => 10, :maximum => 10000 }
     validates :user_id, :presence => true
 end
