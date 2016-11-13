c19dc1123ea4008eaf29c947e61ff7e89aa5f98d
Mark Stevens <railsadvocate@gmail.com>
2016-11-13 00:55:16 -0800

    add category to navbar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
+
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
